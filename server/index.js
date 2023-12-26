e8e41b52cca5e4574a3b8694a9f46b9655adbd88
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2023-12-25 22:29:55 -0700

    MongoDB setup done

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});

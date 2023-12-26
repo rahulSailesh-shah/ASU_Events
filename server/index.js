47573937845c4cffdb3f21f5343e890ad65140f5
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2023-12-25 22:58:03 -0700

    Error handler, Async Handler middleware done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 dotenv.config({ path: "./config/.env" });
 
@@ -11,8 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
 });

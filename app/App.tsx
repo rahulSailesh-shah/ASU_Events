c26b471c9faa5399f7cc600959419a6b363155f2
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2023-12-25 22:30:40 -0700

    React native app setup done

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {Text, View} from 'react-native';
 

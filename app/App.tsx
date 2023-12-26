119690d4faa62aa5e8ffa00c7de5f0a817d5fffd
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2023-12-25 21:58:18 -0700

    initial commit

---
+++ app/App.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <Text>Hello World</Text>;
+}
+
+export default App;
